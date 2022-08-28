4af91cc4050a96452edfb23eeb4d368f6b2d4aa8
Joshua Nathaniel M <clsjoshua@gmail.com>
2022-08-29 02:18:19 +0700

    feat: create constants

---
+++ src/constants/index.tsx
@@ -0,0 +1,8 @@
+export const LIST_TYPES = ["numbered-list", "bulleted-list"];
+export const TEXT_ALIGN_TYPES = ["left", "center", "right", "justify"];
+export const HOTKEYS = {
+  "mod+b": "bold",
+  "mod+i": "italic",
+  "mod+u": "underline",
+  "mod+`": "code",
+};
