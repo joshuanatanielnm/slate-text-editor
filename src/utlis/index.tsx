877be3c38ba13421642e91cdaa5aea0ea539e600
Joshua Nathaniel M <clsjoshua@gmail.com>
2022-08-29 02:18:32 +0700

    feat: create utils function

---
+++ src/utlis/index.tsx
@@ -0,0 +1,72 @@
+import { BaseEditor, Editor, Element as SlateElement, Transforms } from "slate";
+import { LIST_TYPES, TEXT_ALIGN_TYPES } from "../constants";
+
+export const isBlockActive = (
+  editor: BaseEditor,
+  format: string,
+  blockType = "type"
+) => {
+  const { selection } = editor;
+  if (!selection) return false;
+
+  const [match] = Array.from(
+    Editor.nodes(editor, {
+      at: Editor.unhangRange(editor, selection),
+      match: (n) =>
+        !Editor.isEditor(n) &&
+        SlateElement.isElement(n) &&
+        (n as any)[blockType] === format,
+    })
+  );
+
+  return !!match;
+};
+
+export const isMarkActive = (editor: BaseEditor, format: string) => {
+  const marks = Editor.marks(editor);
+  return marks ? (marks as any)[format] === true : false;
+};
+
+export const toggleMark = (editor: BaseEditor, format: string) => {
+  const isActive = isMarkActive(editor, format);
+
+  if (isActive) {
+    Editor.removeMark(editor, format);
+  } else {
+    Editor.addMark(editor, format, true);
+  }
+};
+
+export const toggleBlock = (editor: BaseEditor, format: string) => {
+  const isActive = isBlockActive(
+    editor,
+    format,
+    TEXT_ALIGN_TYPES.includes(format) ? "align" : "type"
+  );
+  const isList = LIST_TYPES.includes(format);
+
+  Transforms.unwrapNodes(editor, {
+    match: (n) =>
+      !Editor.isEditor(n) &&
+      SlateElement.isElement(n) &&
+      LIST_TYPES.includes((n as any).type) &&
+      !TEXT_ALIGN_TYPES.includes(format),
+    split: true,
+  });
+  let newProperties;
+  if (TEXT_ALIGN_TYPES.includes(format)) {
+    newProperties = {
+      align: isActive ? undefined : format,
+    };
+  } else {
+    newProperties = {
+      type: isActive ? "paragraph" : isList ? "list-item" : format,
+    };
+  }
+  Transforms.setNodes<any>(editor, newProperties);
+
+  if (!isActive && isList) {
+    const block = { type: format, children: [] };
+    Transforms.wrapNodes(editor, block);
+  }
+};
